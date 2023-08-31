f8e2bfd47b4bc5fa620fe570512808c242d7f9d3
Prince <princethakur8282@gmail.com>
2023-08-31 12:15:38 +0530

    addind reasons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import Hero from './Components/Hero/Hero'
+import Plans from './Components/Plans/Plans';
+import Programs from './Components/Programs/Programs';
+import Reasons from './Components/Reasons/Reasons';
 function App() {
   return (
     <div className="App">
       <Hero/>
+      <Programs/>
+      <Reasons/>
+      <Plans/>
     </div>
   );
 }
